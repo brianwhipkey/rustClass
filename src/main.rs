1d05e015646e39dcb6aa9ef46abf594b8fad0299
Brian <brianwhipkey@gmail.com>
2024-06-14 18:36:25 -0500

    Item Struct Added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
+#![allow(unused)]
+
+pub enum SizeDesc
+    {
+        Small,
+        Medium,
+        Large
+    }
+
+    pub struct Item
+    {
+        pub id: u16,
+        pub quantity: u32,
+        pub size: SizeDesc 
+    }
+
 fn main() 
 {
     println!("Hello, World!");
+
+    let mut test_item = Item
+    {
+        id: 256,
+        quantity: 56,
+        size: SizeDesc::Small
+    };
+
+    println!("{}",test_item.id);
 }
